f5ac4afdc6cd7b8384e03b7722a0e002c5ceed23
Manoj Kumar <deekondamanojkumar123@gmail.com>
2026-05-19 15:02:47 +0530

    Added task editing functionality to to-do list

--- task2-intermediate/script.js
+++ task2-intermediate/script.js
@@ -30,3 +30,62 @@ form.addEventListener("submit", function(event) {
 
     form.reset();
 });
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+
+const taskInput = document.getElementById("taskInput");
+
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", function() {
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+
+        alert("Please enter a task.");
+
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+    <span class="task-text">${taskText}</span>
+
+    <div>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+    </div>
+   `;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+    const deleteBtn = li.querySelector(".delete-btn");
+    const editBtn = li.querySelector(".edit-btn");
+
+    deleteBtn.addEventListener("click", function() {
+
+        li.remove();
+
+    });
+    editBtn.addEventListener("click", function() {
+
+    const taskSpan = li.querySelector(".task-text");
+
+    const updatedTask = prompt(
+        "Update your task:",
+        taskSpan.textContent
+    );
+
+    if (updatedTask !== null && updatedTask.trim() !== "") {
+
+        taskSpan.textContent = updatedTask;
+
+    }
+
+ });
+
+});
