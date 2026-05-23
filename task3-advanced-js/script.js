ca2bd8bc62150d424c7de5cef925951c7833b90f
Manoj Kumar <deekondamanojkumar123@gmail.com>
2026-05-23 13:36:49 +0530

    Built interactive quiz application using JavaScript

---
+++ task3-advanced-js/script.js
@@ -0,0 +1,79 @@
+const questions = [
+
+    {
+        question: "Which language is used for styling webpages?",
+        answers: ["HTML", "CSS", "Python", "C++"],
+        correct: "CSS"
+    },
+
+    {
+        question: "Which language is used for webpage structure?",
+        answers: ["Java", "HTML", "Python", "C"],
+        correct: "HTML"
+    },
+
+    {
+        question: "Which language is used for webpage interactivity?",
+        answers: ["CSS", "JavaScript", "SQL", "Java"],
+        correct: "JavaScript"
+    }
+
+];
+
+const questionElement = document.getElementById("question");
+
+const answerButtons = document.querySelectorAll(".answer-btn");
+
+const nextBtn = document.getElementById("nextBtn");
+
+let currentQuestionIndex = 0;
+
+function loadQuestion() {
+
+    const currentQuestion = questions[currentQuestionIndex];
+
+    questionElement.textContent = currentQuestion.question;
+
+    answerButtons.forEach((button, index) => {
+
+        button.textContent = currentQuestion.answers[index];
+
+        button.onclick = function() {
+
+            if (button.textContent === currentQuestion.correct) {
+
+                alert("Correct Answer!");
+
+            } else {
+
+                alert("Wrong Answer!");
+
+            }
+
+        };
+
+    });
+
+}
+
+nextBtn.addEventListener("click", function() {
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+
+        loadQuestion();
+
+    } else {
+
+        alert("Quiz Completed!");
+
+        currentQuestionIndex = 0;
+
+        loadQuestion();
+
+    }
+
+});
+
+loadQuestion();
